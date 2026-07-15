f7b942b43aa65af96804240ec8ffccf4f1297311
Yoav Segev <host-yoav@local.com>
2026-07-15 23:56:26 +0300

    feat(CommandResult.java): Implement command result record

--- src/main/java/org/javuntu/shell/services/CommandResult.java
+++ src/main/java/org/javuntu/shell/services/CommandResult.java
@@ -1,4 +1,11 @@
 package org.javuntu.shell.services;
 
-public class CommandResult {
+public record CommandResult(String output) {
+    public static CommandResult text(String output) {
+        return new CommandResult(output);
+    }
+
+    public static CommandResult empty() {
+        return new CommandResult("");
+    }
 }
