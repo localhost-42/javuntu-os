bf5cb18233e55a9c7b0321b7b4805b2bfc11aa9e
Yoav Segev <host-yoav@local.com>
2026-07-16 00:05:32 +0300

    refactor(CommandResult.java): Implement more functunulity to command result

--- src/main/java/org/javuntu/shell/services/CommandResult.java
+++ src/main/java/org/javuntu/shell/services/CommandResult.java
@@ -1,11 +1,19 @@
 package org.javuntu.shell.services;
 
-public record CommandResult(String output) {
+public record CommandResult(String output, boolean shouldClearScreen, boolean shouldExit) {
     public static CommandResult text(String output) {
-        return new CommandResult(output);
+        return new CommandResult(output, false, false);
     }
 
     public static CommandResult empty() {
-        return new CommandResult("");
+        return new CommandResult("", false, false);
+    }
+
+    public static CommandResult clearScreen() {
+        return new CommandResult("", true, false);
+    }
+
+    public static CommandResult exit() {
+        return new CommandResult("", false, true);
     }
 }
