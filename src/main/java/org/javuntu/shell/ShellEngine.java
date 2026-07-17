610edd78ce0dccadb34f9404227c5c9e7e43f189
Yoav Segev <host-yoav@local.com>
2026-07-17 21:48:59 +0300

    feat(ShellEngine.java): Implement Shell Engine init

--- src/main/java/org/javuntu/shell/ShellEngine.java
+++ src/main/java/org/javuntu/shell/ShellEngine.java
@@ -1,4 +1,22 @@
 package org.javuntu.shell;
 
+import org.javuntu.shell.services.CommandContext;
+import org.javuntu.shell.services.CommandLoader;
+import org.javuntu.shell.services.CommandRegistry;
+
+import java.util.List;
+
 public class ShellEngine {
+    private final CommandRegistry commandRegistry;
+    private final CommandContext context;
+
+    public ShellEngine() {
+        List<Command> commands = CommandLoader.loadCommands(ShellConstants.PATH);
+        commandRegistry = new CommandRegistry(commands.toArray(new Command[0]));
+
+        context = new CommandContext();
+    }
+
+
+
 }
