51926b8277305712e9e9ba59db505f62a5e9a886
Yoav Segev <host-yoav@local.com>
2026-07-17 22:15:34 +0300

    feat(ShellEngine.java): Implement command executor for shell engine

--- src/main/java/org/javuntu/shell/ShellEngine.java
+++ src/main/java/org/javuntu/shell/ShellEngine.java
@@ -1,22 +1,39 @@
 package org.javuntu.shell;
 
-import org.javuntu.shell.services.CommandContext;
-import org.javuntu.shell.services.CommandLoader;
-import org.javuntu.shell.services.CommandRegistry;
+import org.javuntu.exceptions.CommandNotExistException;
+import org.javuntu.shell.services.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShellEngine {
-    private final CommandRegistry commandRegistry;
+    private final CommandRegistry registry;
     private final CommandContext context;
 
     public ShellEngine() {
         List<Command> commands = CommandLoader.loadCommands(ShellConstants.PATH);
-        commandRegistry = new CommandRegistry(commands.toArray(new Command[0]));
+        registry = new CommandRegistry(commands.toArray(new Command[0]));
 
         context = new CommandContext();
     }
 
+    public CommandResult execute(String line) {
+        List<String> tokens = CommandParser.parse(line);
 
+        if (tokens.isEmpty()) {
+            return CommandResult.empty();
+        }
+
+        String commandName = tokens.get(0);
+        List<String> args = new ArrayList<>(tokens.subList(1, tokens.size()));
+
+        Command command = registry.getCommand(commandName);
+
+        if (command != null) {
+            return command.execute(context, String.valueOf(args));
+        } else {
+            throw new CommandNotExistException(commandName);
+        }
+    }
 
 }
