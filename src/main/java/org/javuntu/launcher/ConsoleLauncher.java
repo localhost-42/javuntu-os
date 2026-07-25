fa0bed13233524b2fefd9650a64a370d38833c4e
Yoav Segev <host-yoav@local.com>
2026-07-25 18:19:06 +0300

    feat(ConsoleShell.java): Implenemnt run shell architecture for the console ui

--- src/main/java/org/javuntu/launcher/ConsoleLauncher.java
+++ src/main/java/org/javuntu/launcher/ConsoleLauncher.java
@@ -1,11 +1,12 @@
 package org.javuntu.launcher;
 
+import org.javuntu.shell.ShellEngine;
 import org.javuntu.shell.ui.ConsoleShell;
 
 public class ConsoleLauncher implements Launcher {
     @Override
     public void run(String[] args) {
-        ConsoleShell consoleShell = new ConsoleShell();
+        ConsoleShell consoleShell = new ConsoleShell(new ShellEngine());
 
         consoleShell.run();
     }
